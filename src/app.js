eb2207d14c8cf0eb5e0b9f0f2404398213c62e0e
Billy Storm <billstormbiz@gmail.com>
2024-07-01 20:37:40 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,55 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function(form) {
+    form.addEventListener(
+      "submit",
+      function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("paymentForm");
+  const alertDiv = document.getElementById("alertDiv");
+
+  form.addEventListener(
+    "submit",
+    function(event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+        alertDiv.style.display = "block";
+      } else {
+        alertDiv.style.display = "none";
+      }
+
+      form.classList.add("was-validated");
+    },
+    false
+  );
+
+  form.addEventListener("input", function() {
+    if (form.checkValidity()) {
+      alertDiv.style.display = "none";
+    }
+  });
+});
